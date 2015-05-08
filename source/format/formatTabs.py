bf7d444b3798868c3616f638822dbf766a3b3296
damiancclarke <damian.c.clarke@gmail.com>
2015-05-07 23:28:29 -0300

    Format tables v1

--- source/format/formatTabs.py
+++ source/format/formatTabs.py
@@ -24,8 +24,17 @@ print('\n\n Producing tex files for output tables.\n\n')
 RES   = "/home/damiancclarke/investigacion/2015/birthQuarter/results/"
 TAB   = "/home/damiancclarke/investigacion/2015/birthQuarter/tables/"
 
-singleIPUM = RES + 'ipums/sumStats/FullSample.txt'
-singleNVSS = RES + 'nvss/sumStats/FullSample.txt'
+singleIPUM       = RES + 'ipums/sumStats/FullSample.txt'
+singleNVSS       = RES + 'nvss/sumStats/FullSample.txt'
+singleEducIPUM   = RES + 'ipums/sumStats/EducSample.txt'
+singleEducNVSS   = RES + 'nvss/sumStats/EducSample.txt'
+
+
+twinIPUM       = RES + 'ipums/sumStats/FullSampletwins.txt'
+twinNVSS       = RES + 'nvss/sumStats/FullSampletwins.txt'
+twinEducIPUM   = RES + 'ipums/sumStats/EducSampletwins.txt'
+twinEducNVSS   = RES + 'nvss/sumStats/EducSampletwins.txt'
+
 
 
 #==============================================================================
@@ -40,7 +49,7 @@ br   = '\\bottomrule'
 mc1  = '\\multicolumn{'
 mc2  = '}}'
 twid = ['10']
-tcm  = ['}{p{14.0cm}}']
+tcm  = ['}{p{17.0cm}}']
 mc3  = '{\\begin{footnotesize}\\textsc{Notes:} '
 lname = "Fertility$\\times$desire"
 tname = "Twin$\\times$desire"
@@ -53,48 +62,55 @@ R2   = 'R$^2$'
 #==============================================================================
 #== (2) Write sum stat tables
 #==============================================================================
-sumT = open(TAB + "sumSingle.tex", 'w')
+for parity in ['single', 'twin']:
+    sumT = open(TAB + 'sum'+parity+'.tex', 'w')
 
-NV = open(singleNVSS, 'r').readlines()
-IP = open(singleIPUM, 'r').readlines()
+    if parity=='twin':
+        NV  = open(twinNVSS, 'r').readlines()
+        IP  = open(twinIPUM, 'r').readlines()
+        NVe = open(twinEducNVSS, 'r').readlines()
+        IPe = open(twinEducIPUM, 'r').readlines()
+        headline = 'Twins'
+    elif parity=='single':
+        NV  = open(singleNVSS, 'r').readlines()
+        IP  = open(singleIPUM, 'r').readlines()
+        NVe = open(singleEducNVSS, 'r').readlines()
+        IPe = open(singleEducIPUM, 'r').readlines()
+        headline = 'Singletons'
 
-l1a = NV[1].replace("\"", "")
-l2a = NV[2].replace("\"", "")
-
-
-
-sumT.write("\\begin{landscape}\\begin{table}[htpb!]"
-    "\\caption{Percent of Births, Singletons} \n"
-    "\\label{bqTab:singleSum}\\begin{center}"
-    "\\begin{tabular}{lccccp{6mm}cccc}\n\\toprule \\toprule \n"
-    "&\\multicolumn{4}{c}{NVSS}&&\\multicolumn{4}{c}{ACS}"
-    "\\\\ \\cmidrule(r){2-5} \\cmidrule(r){7-10} \n"
-    "& Bad    & Good   & Diff. & Ratio && Bad    & Good   & Diff. & Ratio\\\\\n"
-    "& Season & Season &       &       && Season & Season &     & \\\\\\midrule"
-    "\multicolumn{10}{l}{\\textsc{Panel A: By Age Groups}}\\\\"
-    "\n"+"\\begin{footnotesize}\\end{footnotesize}& \n"*9+
-    "\\begin{footnotesize}\\end{footnotesize}\\\\ \n")
-
-sumT.write(NV[1]+'&&'+IP[1]+'\\\\ \n'
-          +NV[2]+'&&'+IP[2]+'\\\\ \n &&&&&&&&& \\\\'
-
-
-"\multicolumn{10}{l}{\\textsc{Panel B: By Age and Education}}\\\\"
-"\n"+"\\begin{footnotesize}\\end{footnotesize}& \n"*9+
-"\\begin{footnotesize}\\end{footnotesize}\\\\ \n")
-
-
-
-
-sumT.write('\n'+mr+mc1+twid[0]+tcm[0]+mc3+
-"Good season refers to birth quarters 2 and 3 (Apr-Jun and Jul-Sept).  Bad "
-"quarter refers to quarters 1 and 4 (Jan-Mar and Oct-Dec).  Values reflect "
-"the percent of yearly births each season from 2005-2013. `Young' refers to"
-" 20-39 year olds, `Old' refers to 40-45 year olds. \n"
-"\\end{footnotesize}} \\\\ \\bottomrule \n \\end{tabular}\\end{center}"
-"\\end{table}\\end{landscape}")
-
-sumT.close()
+    sumT.write("\\begin{landscape}\\begin{table}[htpb!]"
+               "\\caption{Percent of Births, "+headline+"} \n"
+               "\\label{bqTab:"+parity+"Sum}\\begin{center}"
+               "\\begin{tabular}{lccccp{6mm}cccc}\n\\toprule \\toprule \n"
+               "&\\multicolumn{4}{c}{NVSS}&&\\multicolumn{4}{c}{ACS}"
+               "\\\\ \\cmidrule(r){2-5} \\cmidrule(r){7-10} \n"
+               "& Bad    & Good   & Diff. & Ratio && Bad    & Good   & Diff. & Ratio\\\\\n"
+               "& Season & Season &       &       && Season & Season &     & \\\\\\midrule"
+               "\multicolumn{10}{l}{\\textsc{Panel A: By Age Groups}}\\\\"
+               "\n"+"\\begin{footnotesize}\\end{footnotesize}& \n"*9+
+               "\\begin{footnotesize}\\end{footnotesize}\\\\ \n")
+    
+    sumT.write(NV[1]+'&&'+IP[1]+'\\\\ \n'
+               +NV[2]+'&&'+IP[2]+'\\\\ \n &&&&&&&&& \\\\'
+               "\multicolumn{10}{l}{\\textsc{Panel B: By Age and Education}}\\\\"
+               "\n"+"\\begin{footnotesize}\\end{footnotesize}& \n"*9+
+               "\\begin{footnotesize}\\end{footnotesize}\\\\ \n"+
+               NVe[1]+'&&'+IPe[1]+'\\\\ \n'+
+               NVe[2]+'&&'+IPe[2]+'\\\\ \n'+
+               NVe[3]+'&&'+IPe[3]+'\\\\ \n'+
+               NVe[4]+'&&'+IPe[4]+'\\\\ \n &&&&&&&&& \\\\'
+               )
+    
+    
+    sumT.write('\n'+mr+mc1+twid[0]+tcm[0]+mc3+
+               "Good season refers to birth quarters 2 and 3 (Apr-Jun and Jul-Sept).  Bad "
+               "quarter refers to quarters 1 and 4 (Jan-Mar and Oct-Dec).  Values reflect "
+               "the percent of yearly births each season from 2005-2013. `Young' refers to"
+               " 20-39 year olds, `Old' refers to 40-45 year olds. \n"
+               "\\end{footnotesize}} \\\\ \\bottomrule \n \\end{tabular}\\end{center}"
+               "\\end{table}\\end{landscape}")
+    
+    sumT.close()
 
 
 #==============================================================================
@@ -102,6 +118,10 @@ sumT.close()
 #==============================================================================
 final = open(TAB + "tables.tex", 'w')
 
-final.write("\\input{./../tables/sumSingle.tex} \n")
+final.write("\\input{./../tables/sumsingle.tex} \n")
+final.write("\\input{./../tables/sumtwin.tex} \n")
+final.write("\\begin{landscape}")
+final.write("\\input{./../results/ipums/regressions/IPUMSBinary.tex} \n")
+final.write("\\end{landscape}")
 
 final.close()
